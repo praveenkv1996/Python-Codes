2949ed637b0f26802c8d72853188a5d40d2f2abf
praveenkv1996 <praveenv19@navgurukul.org>
2022-01-20 13:18:33 +0530

    files ready to upload

---
+++ loopssss.py
@@ -0,0 +1,16 @@
+i =1 
+# while i <=10:
+#     print(i)
+#     i+=1
+# print("terminate loops")
+
+
+i = 1
+while i<=5:
+    j = 1
+    while j<i+1:
+        print(j,end="")
+        j=j+1
+    print("\n")
+    i = i+1
+   
--- practice.py
+++ practice.py
@@ -8,194 +8,469 @@
 #     f=a-b+e
 #     print(f)
 
-na=input("Enter Your Name:- ")
-name=na.lower()
-if "a" in name:
-    b1=name.find("a")
-    b2="b"
-    name1=name[:b1]+b2+name[b1+1:]
-    if "e" in name1:
-        f1=name1.find("e")
-        f2="f"
-        name2=name1[:f1]+f2+name1[f1+1:]
-        if "i" in name2:
-            j1=name2.find("i")
-            j2="j"
-            name3=name2[:j1]+j2+name2[j1+1:]
-            if "o" in name3:
-                p1=name3.find("o")
-                p2="p"
-                name4=name3[:p1]+p2+name3[p1+1:]
-                if "u" in name4:
-                    v1=name4.find("u")
-                    v2="v"
-                    name5=name4[:v1]+v2+name4[v1+1:]
-                    print(name5)
-                elif not "u" in name4:
-                    print(name4)
-            elif not "o" in name3:
-                if "u" in name3:
-                    v1=name3.find("u")
-                    v2="v"
-                    name4=name3[:v1]+v2+name3[v1+1:]
-                    print(name4)
-                elif not "u" in name3:
-                    print(name3)
-        elif not "i" in name2:
-            if "o" in name2:
-                p1=name2.find("o")
-                p2="p"
-                name3=name2[:p1]+p2+name2[p1+1:]
-                if "u" in name3:
-                    v1=name3.find("u")
-                    v2="v"
-                    name4=name3[:v1]+v2+name3[v1+1:]
-                    print(name4)
-                elif not "u" in name3:
-                    print(name3)
-            elif not "o" in name2:
-                if "u" in name2:
-                    v1=name2.find("u")
-                    v2="v"
-                    name3=name2[:v1]+v2+name2[v1+1:]
-                    print(name3)
-                elif not "u" in name2:
-                    print(name2)
-    elif not "e" in name1:
-        if "i" in name1:
-            j1=name1.find("i")
-            j2="j"
-            name2=name1[:j1]+j2+name1[j1+1:]
-            if "o" in name2:
-                p1=name2.find("o")
-                p2="p"
-                name3=name2[:p1]+p2+name2[p1+1:]
-                if "u" in name3:
-                    v1=name3.find("u")
-                    v2="v"
-                    name4=name3[:v1]+v2+name3[v1+1:]
-                    print(name4)
-                elif not "u" in name3:
-                    print(name3)
-            elif not "o" in name2:
-                if "u" in name2:
-                    v1=name2.find("u")
-                    v2="v"
-                    name3=name2[:v1]+v2+name2[v1+1:]
-                    print(name3)
-                elif not "u" in name2:
-                    print(name2)
-        elif not "i" in name1:
-            if "o" in name1:
-                p1=name1.find("o")
-                p2="p"
-                name2=name1[:p1]+p2+name1[p1+1:]
-                if "u" in name2:
-                    v1=name2.find("u")
-                    v2="v"
-                    name3=name2[:v1]+v2+name2[v1+1:]
-                    print(name3)
-                elif not "u" in name3:
-                    print(name3)
-            elif not "o" in name1:
-                if "u" in name1:
-                    v1=name1.find("u")
-                    v2="v"
-                    name2=name1[:v1]+v2+name1[v1+1:]
-                    print(name2)
-                elif not "u" in name1:
-                    print(name1)
-if not "a" in name:
-    if "e" in name:
-        f1=name.find("e")
-        f2="f"
-        name1=name[:f1]+f2+name[f1+1:]
-        if "i" in name1:
-            j1=name1.find("i")
-            j2="j"
-            name2=name1[:j1]+j2+name1[j1+1:]
-            if "o" in name2:
-                p1=name2.find("o")
-                p2="p"
-                name3=name2[:p1]+p2+name2[p1+1:]
-                if "u" in name3:
-                    v1=name3.find("u")
-                    v2="v"
-                    name4=name3[:v1]+v2+name3[v1+1:]
-                    print(name4)
-                elif not "u" in name3:
-                    print(name3)
-            elif not "o" in name2:
-                if "u" in name2:
-                    v1=name2.find("u")
-                    v2="v"
-                    name3=name2[:v1]+v2+name2[v1+1:]
-                    print(name3)
-                elif not "u" in name2:
-                    print(name2)
-        elif not "i" in name1:
-            if "o" in name1:
-                p1=name1.find("o")
-                p2="p"
-                name2=name1[:p1]+p2+name1[p1+1:]
-                if "u" in name2:
-                    v1=name2.find("u")
-                    v2="v"
-                    name3=name2[:v1]+v2+name2[v1+1:]
-                    print(name3)
-                elif not "u" in name2:
-                    print(name2)
-            elif not "o" in name1:
-                if "u" in name1:
-                    v1=name1.find("u")
-                    v2="v"
-                    name2=name1[:v1]+v2+name11[v1+1:]
-                    print(name2)
-                elif not "u" in name1:
-                    print(name1)
-    elif not "e" in name:
-        if "i" in name:
-            j1=name.find("i")
-            j2="j"
-            name1=name[:j1]+j2+name[j1+1:]
-            if "o" in name1:
-                p1=name1.find("o")
-                p2="p"
-                name2=name1[:p1]+p2+name1[p1+1:]
-                if "u" in name2:
-                    v1=name2.find("u")
-                    v2="v"
-                    name3=name2[:v1]+v2+name2[v1+1:]
-                    print(name3)
-                elif not "u" in name2:
-                    print(name2)
-            elif not "o" in name1:
-                if "u" in name1:
-                    v1=name1.find("u")
-                    v2="v"
-                    name2=name1[:v1]+v2+name1[v1+1:]
-                    print(name2)
-                elif not "u" in name1:
-                    print(name1)
-        elif not "i" in name:
-            if "o" in name:
-                p1=name.find("o")
-                p2="p"
-                name1=name[:p1]+p2+name[p1+1:]
-                if "u" in name1:
-                    v1=name1.find("u")
-                    v2="v"
-                    name2=name1[:v1]+v2+name1[v1+1:]
-                    print(name2)
-                elif not "u" in name2:
-                    print(name2)
-            elif not "o" in name:
-                if "u" in name:
-                    v1=name.find("u")
-                    v2="v"
-                    name1=name[:v1]+v2+name[v1+1:]
-                    print(name1)
-                elif not "u" in name:
-                    print(name)
+# na=input("Enter Your Name:- ")
+# name=na.lower()
+# if "a" in name:
+#     b1=name.find("a")
+#     b2="b"
+#     name1=name[:b1]+b2+name[b1+1:]
+#     if "e" in name1:
+#         f1=name1.find("e")
+#         f2="f"
+#         name2=name1[:f1]+f2+name1[f1+1:]
+#         if "i" in name2:
+#             j1=name2.find("i")
+#             j2="j"
+#             name3=name2[:j1]+j2+name2[j1+1:]
+#             if "o" in name3:
+#                 p1=name3.find("o")
+#                 p2="p"
+#                 name4=name3[:p1]+p2+name3[p1+1:]
+#                 if "u" in name4:
+#                     v1=name4.find("u")
+#                     v2="v"
+#                     name5=name4[:v1]+v2+name4[v1+1:]
+#                     print(name5)
+#                 elif not "u" in name4:
+#                     print(name4)
+#             elif not "o" in name3:
+#                 if "u" in name3:
+#                     v1=name3.find("u")
+#                     v2="v"
+#                     name4=name3[:v1]+v2+name3[v1+1:]
+#                     print(name4)
+#                 elif not "u" in name3:
+#                     print(name3)
+#         elif not "i" in name2:
+#             if "o" in name2:
+#                 p1=name2.find("o")
+#                 p2="p"
+#                 name3=name2[:p1]+p2+name2[p1+1:]
+#                 if "u" in name3:
+#                     v1=name3.find("u")
+#                     v2="v"
+#                     name4=name3[:v1]+v2+name3[v1+1:]
+#                     print(name4)
+#                 elif not "u" in name3:
+#                     print(name3)
+#             elif not "o" in name2:
+#                 if "u" in name2:
+#                     v1=name2.find("u")
+#                     v2="v"
+#                     name3=name2[:v1]+v2+name2[v1+1:]
+#                     print(name3)
+#                 elif not "u" in name2:
+#                     print(name2)
+#     elif not "e" in name1:
+#         if "i" in name1:
+#             j1=name1.find("i")
+#             j2="j"
+#             name2=name1[:j1]+j2+name1[j1+1:]
+#             if "o" in name2:
+#                 p1=name2.find("o")
+#                 p2="p"
+#                 name3=name2[:p1]+p2+name2[p1+1:]
+#                 if "u" in name3:
+#                     v1=name3.find("u")
+#                     v2="v"
+#                     name4=name3[:v1]+v2+name3[v1+1:]
+#                     print(name4)
+#                 elif not "u" in name3:
+#                     print(name3)
+#             elif not "o" in name2:
+#                 if "u" in name2:
+#                     v1=name2.find("u")
+#                     v2="v"
+#                     name3=name2[:v1]+v2+name2[v1+1:]
+#                     print(name3)
+#                 elif not "u" in name2:
+#                     print(name2)
+#         elif not "i" in name1:
+#             if "o" in name1:
+#                 p1=name1.find("o")
+#                 p2="p"
+#                 name2=name1[:p1]+p2+name1[p1+1:]
+#                 if "u" in name2:
+#                     v1=name2.find("u")
+#                     v2="v"
+#                     name3=name2[:v1]+v2+name2[v1+1:]
+#                     print(name3)
+#                 elif not "u" in name3:
+#                     print(name3)
+#             elif not "o" in name1:
+#                 if "u" in name1:
+#                     v1=name1.find("u")
+#                     v2="v"
+#                     name2=name1[:v1]+v2+name1[v1+1:]
+#                     print(name2)
+#                 elif not "u" in name1:
+#                     print(name1)
+# if not "a" in name:
+#     if "e" in name:
+#         f1=name.find("e")
+#         f2="f"
+#         name1=name[:f1]+f2+name[f1+1:]
+#         if "i" in name1:
+#             j1=name1.find("i")
+#             j2="j"
+#             name2=name1[:j1]+j2+name1[j1+1:]
+#             if "o" in name2:
+#                 p1=name2.find("o")
+#                 p2="p"
+#                 name3=name2[:p1]+p2+name2[p1+1:]
+#                 if "u" in name3:
+#                     v1=name3.find("u")
+#                     v2="v"
+#                     name4=name3[:v1]+v2+name3[v1+1:]
+#                     print(name4)
+#                 elif not "u" in name3:
+#                     print(name3)
+#             elif not "o" in name2:
+#                 if "u" in name2:
+#                     v1=name2.find("u")
+#                     v2="v"
+#                     name3=name2[:v1]+v2+name2[v1+1:]
+#                     print(name3)
+#                 elif not "u" in name2:
+#                     print(name2)
+#         elif not "i" in name1:
+#             if "o" in name1:
+#                 p1=name1.find("o")
+#                 p2="p"
+#                 name2=name1[:p1]+p2+name1[p1+1:]
+#                 if "u" in name2:
+#                     v1=name2.find("u")
+#                     v2="v"
+#                     name3=name2[:v1]+v2+name2[v1+1:]
+#                     print(name3)
+#                 elif not "u" in name2:
+#                     print(name2)
+#             elif not "o" in name1:
+#                 if "u" in name1:
+#                     v1=name1.find("u")
+#                     v2="v"
+#                     name2=name1[:v1]+v2+name11[v1+1:]
+#                     print(name2)
+#                 elif not "u" in name1:
+#                     print(name1)
+#     elif not "e" in name:
+#         if "i" in name:
+#             j1=name.find("i")
+#             j2="j"
+#             name1=name[:j1]+j2+name[j1+1:]
+#             if "o" in name1:
+#                 p1=name1.find("o")
+#                 p2="p"
+#                 name2=name1[:p1]+p2+name1[p1+1:]
+#                 if "u" in name2:
+#                     v1=name2.find("u")
+#                     v2="v"
+#                     name3=name2[:v1]+v2+name2[v1+1:]
+#                     print(name3)
+#                 elif not "u" in name2:
+#                     print(name2)
+#             elif not "o" in name1:
+#                 if "u" in name1:
+#                     v1=name1.find("u")
+#                     v2="v"
+#                     name2=name1[:v1]+v2+name1[v1+1:]
+#                     print(name2)
+#                 elif not "u" in name1:
+#                     print(name1)
+#         elif not "i" in name:
+#             if "o" in name:
+#                 p1=name.find("o")
+#                 p2="p"
+#                 name1=name[:p1]+p2+name[p1+1:]
+#                 if "u" in name1:
+#                     v1=name1.find("u")
+#                     v2="v"
+#                     name2=name1[:v1]+v2+name1[v1+1:]
+#                     print(name2)
+#                 elif not "u" in name2:
+#                     print(name2)
+#             elif not "o" in name:
+#                 if "u" in name:
+#                     v1=name.find("u")
+#                     v2="v"
+#                     name1=name[:v1]+v2+name[v1+1:]
+#                     print(name1)
+#                 elif not "u" in name:
+#                     print(name)
 
+# a=1
+# while a<=100:
+#     print(a)
+#     a=a+1    
+
+# a=7
+# while a<=98:
+#     print(a)
+#     a=a+7
+
+# a=1
+# b=0
+# while a<=100:
+#     b=b+a
+#     a=a+1
+# print(b)
+
+# a=1
+# sum=0
+# while a<=10:
+#     b=int(input("Enter Number:- "))
+#     sum=sum+b
+#     a=a+1
+#     print(b)
+# print(sum)
+
+# a=1
+# while a<=100:
+#     if a%2!=0:
+#         print(a,end=" ")
+#     elif a%2==0:
+#         print(a*-1,end=" ")
+#     a=a+1
+
+# counter=891
+# while counter>=851:
+#     z=891-counter
+#     if z%3==0 and z!=0:
+#         print(z)
+#     counter=counter-1
+
+# name=input("Enter Name:- ")
+# a=name.lower()
+# if "a" in a:
+#     b1=a.index("a")
+#     b2="b"
+#     a=a[:b1]+b2+a[b1+1:]
+# if "e" in a:
+#     f1=a.index("e")
+#     f2="f"
+#     a=a[:f1]+f2+a[f1+1:]
+# if "i" in a:
+#     j1=a.index("i")
+#     j2="j"
+#     a=a[:j1]+j2+a[j1+1:]
+# if "o" in a:
+#     p1=a.index("o")
+#     p2="p"
+#     a=a[:p1]+p2+a[p1+1:]
+# if "u" in a:
+#     v1=a.index("u")
+#     v2="v"
+#     a=a[:v1]+v2+a[v1+1:]
+# print(a)
+
+# counter=556
+# while counter<=656:
+#     a=counter-555
+#     if a%7==0:
+#         print(a)
+#     counter=counter+1
+
+# counter=1
+# while counter<=7:
+#     print(counter*"#")
+#     counter=counter+1
+
+# counter=1
+# while counter<=100:
+#     if counter%3==0 and counter%7==0:
+#         print("NavGurukul")
+#     elif counter%7==0:
+#         print("Gurukul")
+#     elif counter%3==0:
+#         print("Nav")
+#     else:
+#         print(counter)
+#     counter=counter+1
+
+# counter=1
+# a=1
+# sum=0
+# while counter<=10:
+#     print("Step",a,"of 10:-Enter Value>>",end="")
+#     a2=int(input())
+#     sum=sum+a2
+#     a=a+1
+#     counter=counter+1
+# print("Total is",sum)
+
+# counter=156
+# while counter<=156+90:
+#     a=counter-145
+#     print(a)
+#     counter=counter+1
+
+# counter=156
+# while counter>=66:
+#     a=167-counter
+#     print(a)
+#     counter=counter-1
+
+# n=int(input("Enter Value:-"))
+# a=1
+# sum=0
+# while a<=n:
+#     print("step",a,"of",n,":-Enter Value:-",end="")
+#     b=int(input())
+#     sum=sum+b
+#     a=a+1
+# print("total", sum)
+
+# num=int(input("Enter Number>>"))
+# a=1
+# sum=0
+# while a<=num:
+#     if num%a==0 and a>1:
+#         sum=sum+1
+#     a+=1
+# if sum==1:
+#     print(num,"is a prime number")
+# else:
+#     print(num,"is not a prime number")
+
+# num=int(input("Enter Number>>"))
+# a=1
+# while num>=1:
+#     num2=str(num)
+#     print(num2*5)
+#     num-=1
+
+# index=0
+# while 1:
+#     print(index," ",end= "")
+#     index=index+1
+#     if index == 10:
+#         break
+# print("Found at",index,"location")
+
+# counter=20
+# while counter<=100:
+#     if counter%2==0:
+#         print(counter)
+#     counter+=1
+
+# counter=1
+# while counter<=100:
+#     if counter%7==0:
+#         print(counter)
+#     counter+=1
+
+# counter=1
+# a=12
+# sum=0
+# while counter<=421:
+#     if a>421:
+#         break
+#     sum=sum+a
+#     a+=1
+# print(sum)
+
+# a=30
+# sum=0
+# while a<=420:
+#     if a%8==0:
+#         sum=sum+a
+#     a+=1
+# print(sum)
+
+# counter=1
+# sum=0
+# while counter<=11:
+#     print("Enter",counter,"person weight",end="")
+#     weight=float(input("Enter Your Weight>>"))
+#     sum=sum+weight
+#     counter+=1
+# average=int(int(sum)/11)
+# if average%5==0:
+#     print("Average is",average,"and devided by 5")
+# else:
+#     print("Average is",average,"and not devided by 5")
+
+# counter=1
+# while counter<=100:
+#     if counter%2==0:
+#         print(counter*-1,end=" ")
+#     else:
+#         print(counter,end=" ")
+#     counter+=1
+
+# counter=1
+# while counter<=5:
+#     print("Welcome to Guessing Game")
+#     num=int(input("Enter You Number between 1 to 10"))
+#     if num==5:
+#         break
+#     else:
+#         print("Try again")
+#     counter+=1
+
+# x = 654
+# y = 1
+# z = 0
+
+# while z<=100:
+#     print(z)
+#     x=y
+#     y=z
+#     z=x+y
     
+
+# for i in range(100):
+#     a=1
+#     coun=0
+#     while a <= i:
+#         if i%a==0:
+#             coun+=1
+#         a+=1
+#     if coun==2:
+#         print(i)
+
+# import random
+# a=random.randrange(1,10)
+# b=1
+# print("Welome to Guessing Game")
+# while b<=5:
+#     num=int(input("Please Enter Your Guess Number>>"))
+#     if num==a:
+#         print("wow you guess the correct number")
+#         break
+#     elif num<a:
+#         print("Your guess is less than the correct number, try one more time")
+#     else:
+#         print("Your guess is greater than the correct number,try one more time")
+#     b+=1
+
+# num1=int(input("Please Enter 1st Number>>"))
+# num2=int(input("Please Enter 2nd Number>>"))
+# a=1
+# sum=0
+# while a<=num2:
+#     sum+=num1
+#     a+=1
+# print(sum)
+
+# print(1.2*"praveen")
+
+# fac=int(input())
+# a=1
+# c=1
+# while a<=fac:
+#     c*=a
+#     a+=1
+# print(c)
+
+b=0
+c=0
+while True:
+    num=int(input("Please Enter Number>>"))
+    b+=1
+    c+=num
+    print("press q if you want to quit",end="")
+    a=input()
+    if a=="q":
+        break
+print(c//b)
